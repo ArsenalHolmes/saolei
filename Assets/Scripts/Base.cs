677273c50dd2da0bf3c2ddd8bf51df71872426a9
lihao <909799631@qq.com>
2017-07-17 14:21:17 +0800

    初始化上传

---
+++ Assets/Scripts/Base.cs
@@ -0,0 +1,127 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+using System;
+using System.Collections;
+
+public enum BoxStat
+{
+    None,
+    Number,
+    Bomb,
+    Flag,
+}
+
+public class Base : MonoBehaviour,IPointerClickHandler {
+    public int x;
+    public int y;
+    Image image;
+    Text text;
+    int BomNum;
+    bool isClick=false;
+
+    public BoxStat Bs=BoxStat.None;
+    BoxStat TempBs;
+    public List<Base> aroundBaseList = new List<Base>();
+    private void Awake()
+    {
+        image = GetComponent<Image>();
+        text = GetComponentInChildren<Text>();
+        InitManger.Instance.BackFunc += Init;
+    }
+    // Use this for initialization
+    void Start () {
+	
+	}
+    void Init()
+    {
+        
+        BomNum = 0;
+        //设置默认图片
+        //关闭文字显示
+        text.enabled = false;
+        //获取周围格子
+        if (Bs!=BoxStat.Bomb)
+        {
+            aroundBaseList = InitManger.Instance.ReturnBaseList(x, y);
+            foreach (var item in aroundBaseList)
+            {
+                if (item.Bs == BoxStat.Bomb)
+                {
+                    BomNum++;
+                }
+            }
+            //根据雷数改变格子状态
+            if (BomNum != 0)
+            {
+                Bs = BoxStat.Number;
+            }
+        }
+        TempBs = Bs;
+
+    }
+
+    public void OnPointerClick(PointerEventData eventData)
+    {
+        //TODO 点击事件
+        if (PointerEventData.InputButton.Left==eventData.button&&Bs!=BoxStat.Flag)
+        {
+            //左键单击
+            StartCoroutine(ClickEvent());
+        }
+        else if (PointerEventData.InputButton.Right == eventData.button)
+        {
+            //右键点击
+            if (Bs==BoxStat.Flag)
+            {
+                Bs = TempBs;
+                //TODO 回复图片
+                image.color = Color.white;
+            }
+            else
+            {
+                Bs = BoxStat.Flag;
+                //TODO 旗帜图片
+                image.color = Color.blue;
+            }
+            
+        }
+        
+
+    }
+    public IEnumerator ClickEvent()
+    {
+        yield return new WaitForFixedUpdate();
+        if (isClick==false)
+        {
+            isClick = true;
+            if (Bs == BoxStat.None)
+            {
+                //周围所有都格子都点雷
+                foreach (var item in aroundBaseList)
+                {
+                    StartCoroutine(item.ClickEvent());
+                }
+                //TODO 图片
+                image.color = Color.black;
+            }
+            else if (Bs == BoxStat.Number)
+            {
+                //如果点中数字 TODO 
+                text.enabled = true;
+                text.text = BomNum.ToString();
+                image.color=Color.white;
+            }
+            else
+            {
+                //如果是雷
+                //TODO 游戏结束
+                image.color = Color.red;
+            }
+        }
+        
+        
+    }
+
+}
---
+++ Assets/Scripts/InitManger.cs
@@ -0,0 +1,74 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using System;
+
+public class InitManger : MonoBehaviour {
+
+    public int hig=16;
+    public int len=16;
+    Base[,] baseArr;
+    GameObject BasePre;
+    Transform Parent;
+    public Action BackFunc;
+    int BomNum;
+    public int FlagNum;
+    
+    public static InitManger Instance;
+    private void Awake()
+    {
+        Instance = this;
+        BasePre = Resources.Load<GameObject>("Base");
+        Parent = GameObject.Find("Canvas/Panel/Panel").transform;
+        baseArr = new Base[hig, len];
+        StartCoroutine(InitBase());
+    }
+    IEnumerator InitBase()
+    {
+        yield return new WaitForFixedUpdate();
+        for (int i = 0; i < len; i++)
+        {
+            for (int j = 0; j < hig; j++)
+            {
+                Base b = Instantiate(BasePre).AddComponent<Base>();
+                b.transform.SetParent(Parent);
+                b.gameObject.name = i + "--" + j;
+                baseArr[j, i] = b;
+                b.x = i;
+                b.y = j;
+
+                if (UnityEngine.Random.Range(0,5)==0)
+                {
+                    b.Bs = BoxStat.Bomb;
+                }
+            }
+        }
+        BackFunc();
+    }
+    /// <summary>
+    /// 根据格子获得周围格子
+    /// </summary>
+    /// <param name="x"></param>
+    /// <param name="y"></param>
+    /// <returns></returns>
+    public List<Base> ReturnBaseList(int x,int y)
+    {
+        List<Base> LB = new List<Base>();
+        for (int i = -1; i <=1; i++)
+        {
+            for (int j = -1; j <=1; j++)
+            {
+                if (x+i>=0&&x+i<len&&y+j>=0&&y+j<hig)
+                {
+                    if (i==0&&j==0)
+                    {
+                        continue;
+                    }
+                    LB.Add(baseArr[y + j, x + i]);
+                }
+            }
+        }
+        return LB;
+    }
+    //TODO 后续 当旗子数量和位置相同时。判断输赢
+}
